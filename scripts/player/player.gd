1abfad7069ea59e910468e7e761318f1b0773afd
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-09-14 19:12:24 +0200

    Add jump logic

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -21,18 +21,24 @@ func _unhandled_input(event: InputEvent) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
 func _physics_process(delta: float) -> void:
-	const SPEED = 5.5
+	const SPEED := 5.5
 	
-	var input_direction_2d = Input.get_vector(
+	var input_direction_2d := Input.get_vector(
 		"move_left", "move_right", "move_forward", "move_back"
 	)
-	var input_direction_3d = Vector3(
+	var input_direction_3d := Vector3(
 		input_direction_2d.x, 0.0, input_direction_2d.y
 	)
-	var direction = transform.basis * input_direction_3d
+	var direction := transform.basis * input_direction_3d
 	
 	velocity.x = direction.x * SPEED
 	velocity.z = direction.z * SPEED
+	velocity.y -= 20.0 * delta
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y += 10.0
+	elif Input.is_action_just_released("jump") and velocity.y > 0.0:
+		velocity.y = 0.0
 	
 	move_and_slide()
 		
