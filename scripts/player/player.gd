06f14f73006ad9182b327e5e0f109396730a0aed
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-09-14 18:34:46 +0200

    Hide mouse cursor

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -7,6 +7,9 @@ const x_rotation_sensitivity = 0.5
 const x_rotation_min_limit = -80
 const x_rotation_max_limit = 80
 
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotation_degrees.y -= event.relative.x * y_rotation_sensitivity
@@ -14,3 +17,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			camera.rotation_degrees.x - event.relative.y * x_rotation_sensitivity,
 			 x_rotation_min_limit,
 			 x_rotation_max_limit)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	
