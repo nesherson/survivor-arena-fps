a2f135c93840d2655ed2d7ba213f1eb3f72505d7
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-09-16 21:47:15 +0200

    Add inital enemy logic
    - Get enemy to follow the player
    - Add enemy hit logic and animation
    - Add enemy death logic and animation

---
+++ scripts/enemies/mob.gd
@@ -0,0 +1,38 @@
+extends RigidBody3D
+
+@onready var bat_model = %bat_model
+@onready var player = get_node("/root/Game/Player")
+@onready var timer = $Timer
+
+const SPEED := 2.5
+
+var health := 3
+
+func _physics_process(delta: float) -> void:
+	var direction_to_player := global_position.direction_to(player.global_position)
+	
+	direction_to_player.y = 0.0
+	linear_velocity = direction_to_player * SPEED
+	bat_model.rotation.y = Vector3.FORWARD.signed_angle_to(direction_to_player, Vector3.UP) + PI
+
+func take_damage() -> void:
+	if health == 0:
+		return
+		
+	bat_model.hurt()
+	
+	health -= 1
+	
+	if health == 0:
+		set_physics_process(false)
+		gravity_scale = 1.0
+		
+		var direction := -1.0 * global_position.direction_to(player.global_position)
+		var random_upward_force = Vector3.UP * randf_range(1.0, 5.0)
+		
+		apply_central_impulse(direction * 5.0 + random_upward_force)
+		timer.start()
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
